455e67cddf830d7ed41c446a57df642ceb5239a0
playerzer0-ui <playerzero745@gmail.com>
2024-03-01 10:04:47 +0000

    added 2 functions to the messageDao

--- src/test/java/daos/MessageDaoTest.java
+++ src/test/java/daos/MessageDaoTest.java
@@ -17,7 +17,7 @@ class MessageDaoTest {
      */
     @Test
     void getMessage_normal() {
-        Message exp = new Message(1, 1, 1, "hello", 1, LocalDateTime.of(2024, 1, 31, 21, 57, 14), 0);
+        Message exp = new Message(1, 1, 1, "hello", 1, LocalDateTime.of(2024, 1, 31, 21, 57, 14), 0, 0, null);
         Message act = messageDao.getMessage(1);
 
         assertEquals(exp, act);
@@ -73,8 +73,8 @@ class MessageDaoTest {
     @Test
     void getMessages_normal() {
         ArrayList<Message> messages = messageDao.getMessages(1);
-        Message exp = new Message(1, 1, 1, "hello", 1, LocalDateTime.of(2024, 1, 31, 21, 57, 14), 0);
-        Message exp1 = new Message(2, 1, 2, "hi", 1, LocalDateTime.of(2024,1,31,21,58,28), 0);
+        Message exp = new Message(1, 1, 1, "hello", 1, LocalDateTime.of(2024, 1, 31, 21, 57, 14), 0, 0, null);
+        Message exp1 = new Message(2, 1, 2, "hi", 1, LocalDateTime.of(2024,1,31,21,58,28), 0, 0, null);
 
         ArrayList<Message> expected = new ArrayList<>();
         expected.add(exp);
@@ -113,5 +113,38 @@ class MessageDaoTest {
         int act = messageDao.deleteMessages(60);
 
         assertEquals(0, act);
+    }
+
+    /**
+     * getDailyMessageCount, there is one new message in the day
+     */
+    @Test
+    void getDailyMessageCount_normal(){
+        messageDao.sendMessage(1, 1, "asdasdad", 1);
+        int act = messageDao.getDailyMessageCount();
+        messageDao.deleteMessages(6);
+        messageDao.updateIncrement("messages", 5);
+
+        assertEquals(1, act);
+    }
+
+    /**
+     * getDailyMessageCount, no messages in today
+     */
+    @Test
+    void getDailyMessageCount_noMessage(){
+        int act = messageDao.getDailyMessageCount();
+
+        assertEquals(0, act);
+    }
+
+    /**
+     * getTotalMessageCount, normal scenario, 5
+     */
+    @Test
+    void getTotalMessageCount_normal(){
+        int act = messageDao.getTotalMessageCount();
+
+        assertEquals(5, act);
     }
 }
